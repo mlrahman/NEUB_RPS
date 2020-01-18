c93f94b79510287b602b2ed3530f4542a5fe0ec2
Mir Lutfur Rahman <mirlutfur.rahman@gmail.com>
2020-01-19 02:02:28 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,151 @@
+<!-- 
+Jan 18, 2020
+Developed By:
+------------- 
+Mir Lutfur Rahman
+Lecturer
+Department of Computer Science & Engineering
+North East University Bangladesh
+-->
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#"  lang="en-gb">
+	<head>
+		<base href="#"/>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="North,East,University,Bangladesh,Sylhet,Result,NEUB,Portal" />
+		<meta name="description" content="NEUB Result Portal" />
+		<meta name="generator" content="NEUB Result Portal"/>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="images/system/logo.png">
+		<title>NEUB Result Portal</title>
+		<link rel="stylesheet" href="css/w3.css">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/welcome_video.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="js/main.js"></script> 
+	</head>
+	<body class="w3-black">
+		<div class="w3-content w3-white" style="max-width:2000px;">
+			
+			<!-- Navbar (sit on top) -->
+			<div class="w3-container w3-margin-0 w3-padding-0">
+				<div class="w3-bar w3-black w3-card w3-hide-medium w3-hide-small">
+					<a href="https://www.neub.edu.bd" class="w3-bar-item">
+						<image src="images/system/logo.png" alt="NEUB LOGO" class="w3-image" style="width:100%;max-width:90px;">
+					</a>
+					<a href="index.php" class="w3-bar-item w3-jumbo w3-decoration-null">NEUB Result Portal</a>
+					
+				</div>
+				<div class="w3-bar w3-black w3-card w3-hide-large w3-hide-small">
+					<a href="https://www.neub.edu.bd" class="w3-bar-item">
+						<image src="images/system/logo.png" alt="NEUB LOGO" class="w3-image" style="width:100%;max-width:75px;">
+					</a>
+					</br>
+					<a href="index.php" class="w3-bar-item w3-xxxlarge w3-decoration-null" style="margin-top: -15px;">  NEUB Result Portal</a>
+					
+				</div>
+				<div class="w3-bar w3-black w3-card w3-hide-large w3-hide-medium">
+					<a href="https://www.neub.edu.bd" class="w3-bar-item">
+						<image src="images/system/logo.png" alt="NEUB LOGO" class="w3-image" style="width:100%;max-width:55px;">
+					</a>
+					</br>
+					<a href="index.php" class="w3-bar-item w3-xxlarge w3-decoration-null" style="margin-top: -15px;">  NEUB Result Portal</a>
+					
+				</div>
+			</div>
+			
+			
+			
+			<!-- Header with image -->
+			<header class="w3-display-container w3-grayscale-min w3-dark-gray w3-hover-teal" style="height:450px;overflow:hidden;">
+				<img src="images/system/video_alt.jpg" class="w3-image w3-grayscale w3-opacity-min" alt="Video Alternate" style="height:100%;width:100%;"/>
+				
+				<video autoplay muted loop id="myVideo" class="w3-grayscale" >
+					<source src="images/system/welcome.mp4" type="video/mp4"><img src="images/system/video_alt.jpg" class="w3-image w3-grayscale w3-opacity-min" alt="Video Alternate" style="height:100%;width:100%;"/>
+				</video>
+				
+				<div class="w3-display-bottomleft w3-container w3-padding-16 w3-black">
+					Permanent Campus
+				</div>
+				<div class="w3-display-middle w3-center w3-cursor">
+					<div class="w3-container w3-round-large w3-hide-small" style="width: 700px;padding:0px;margin-top:-90px;">
+						<p class="w3-left-align w3-bold" style="padding: 0px; margin:0px 0px 5px 55px; font-size:22px;">Search Your Result:</p>
+						<input class="w3-border w3-border-black w3-round-large" placeholder=" Student ID" type="number" style="margin: 0px;padding:10px;width:235px;" autocomplete="off">
+						<input class="w3-border w3-border-black w3-round-large" placeholder=" Date of Birth (DD-MM-YYYY)" type="text" style="margin: 0px;padding:10px;width:235px;" autocomplete="off">
+						<a class=" w3-black w3-round-large w3-hover-teal" href="#" style="margin: 0px;padding:12px;text-decoration: none;">
+							<i class="fa fa-search"></i> Get Result
+						</a>
+					</div>
+					<div class="w3-container w3-round-large w3-hide-medium w3-hide-large " style="width: 350px;padding:0px;margin-top:-90px;height:auto;">
+						<p class="w3-left-align w3-bold" style="padding: 0px; margin:0px 0px 5px 62px; font-size:20px;">Search Your Result:</p>
+						<input class="w3-border w3-border-black w3-round-large" placeholder=" Student ID" type="number" style="margin: 5px 0px;padding:10px;width:235px;" autocomplete="off"/>
+						</br>
+						<input class="w3-border w3-border-black w3-round-large" placeholder=" Date of Birth (DD-MM-YYYY)" type="text" style="margin: 5px 0px 20px 0px;padding:10px;width:235px;" autocomplete="off"/>
+						</br>
+						<a class=" w3-black w3-round-large w3-hover-teal" href="#" style="margin: 0px;padding:12px;text-decoration: none;">
+							<i class="fa fa-search"></i> Get Result
+						</a>
+					</div>
+					
+					
+				</div>
+			</header>
+			
+			<!-- Footer -->
+			<footer class=" w3-center w3-black">
+				<div class="w3-container w3-white w3-padding-32">
+					<div class="w3-row">
+						<p class="w3-hide-medium w3-hide-large"> &nbsp </p>
+						<div class="w3-third w3-container w3-mobile w3-left-align">
+							<p style="margin:0px 15px;"><b>FIND US ON MAP</b></p>
+							<p style="margin:0px 15px;width:50px;" class="w3-bottombar w3-border-teal"> </p>
+							<table class="w3-table " style="width:100%;max-width:350px;">
+								<tr>
+									<td>Address:</td><td>Telihaor, Sheikhghat, Sylhet-3100</td>
+								</tr>
+								<tr>
+									<td>Tel:</td><td>0821 710221-2</td>
+								</tr>
+								<tr>
+									<td>Email:</td><td><a href="mailto:info@neub.edu.bd" title="NEUB" target="_blank" class="w3-hover-text-teal w3-decoration-null">info@neub.edu.bd</a></td>
+								</tr>
+								<tr>
+									<td>Mobile:</td><td>01755566994</td>
+								</tr>
+								<tr>
+									<td>Web:</td><td><a href="https://www.neub.edu.bd" title="NEUB" target="_blank" class="w3-hover-text-teal w3-decoration-null">www.neub.edu.bd</a></td>
+								</tr>
+							</table>
+						</div>
+						<p class="w3-hide-medium w3-hide-large"> &nbsp </p>
+						<div class="w3-third w3-container w3-mobile w3-left-align">
+							<p style="margin:0px 15px;"><b>FIND US ON MAP</b></p>
+							<p style="margin:0px 15px;width:50px;" class="w3-bottombar w3-border-teal"> </p>
+							<div class="w3-container w3-padding-16" style="width:100%;max-width:400px; height:210px;"><div class="gmap_canvas"><iframe id="gmap_canvas" style="height:100%;width:100%;" src="https://maps.google.com/maps?q=north%20east%20university%20bangladesh&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div></div>
+						</div>
+						<div style="padding: 0px 0px 0px 50px;" class="w3-third w3-container w3-mobile w3-left-align w3-hide-small w3-hide-medium">
+							<p style="margin:0px;"><b>CONTACT US</b></p>
+							<p style="margin:0px;width:50px;" class="w3-bottombar w3-border-teal"> </p>
+							<input class="w3-input w3-animate-input w3-border-teal" placeholder="Name" type="text" style="width:80%;max-width:100%;margin:10px 0px;" autocomplete="off">
+							<input class="w3-input w3-animate-input w3-border-teal" placeholder="Email" type="text" style="width:80%;max-width:100%;margin:10px 0px;" autocomplete="off">
+							<input class="w3-input w3-animate-input w3-border-teal" placeholder="Message" type="text" style="width:80%;max-width:100%;margin:10px 0px;" autocomplete="off">
+							<button class="w3-button w3-right w3-round w3-black w3-hover-teal"><i class="fa fa-send"> Submit</i></button>
+						</div>
+						<div style="padding: 0px 0px 0px 30px;" class="w3-third w3-container w3-mobile w3-left-align w3-hide-large">
+							<p style="margin:0px;"><b>CONTACT US</b></p>
+							<p style="margin:0px;width:50px;" class="w3-bottombar w3-border-teal"> </p>
+							<input class="w3-input w3-animate-input w3-border-teal" placeholder="Name" type="text" style="width:80%;max-width:100%;margin:10px 0px;" autocomplete="off">
+							<input class="w3-input w3-animate-input w3-border-teal" placeholder="Email" type="text" style="width:80%;max-width:100%;margin:10px 0px;" autocomplete="off">
+							<input class="w3-input w3-animate-input w3-border-teal" placeholder="Message" type="text" style="width:80%;max-width:100%;margin:10px 0px;" autocomplete="off">
+							<button class="w3-button w3-right w3-round w3-black w3-hover-teal"><i class="fa fa-send"> Submit</i></button>
+						</div>
+					</div>
+				</div>
+				<p class="w3-cursor w3-hide-medium w3-hide-small w3-large w3-margin">Copyright &copy <?php echo DATE("Y"); ?> <a href="https://www.neub.edu.bd" title="NEUB" target="_blank" class="w3-hover-text-teal w3-decoration-null">North East University Bangladesh.</a></p>
+				<p class="w3-cursor w3-hide-large w3-hide-small w3-medium w3-margin">Copyright &copy <?php echo DATE("Y"); ?> <a href="https://www.neub.edu.bd" title="NEUB" target="_blank" class="w3-hover-text-teal w3-decoration-null">North East University Bangladesh.</a></p>
+				<p class="w3-cursor w3-hide-medium w3-hide-large w3-small w3-margin">Copyright &copy <?php echo DATE("Y"); ?> <a href="https://www.neub.edu.bd" title="NEUB" target="_blank" class="w3-hover-text-teal w3-decoration-null">North East University Bangladesh.</a></p>
+			</footer>
+		</div>
+	</body>
+</html>
