3cbad3b731067af9672d2b64dd4ddf93db2f86d7
Mir Lutfur Rahman <mirlutfur.rahman@gmail.com>
2020-03-23 00:42:07 +0600

    22.03.2020

--- includes/faculty/left_column.php
+++ includes/faculty/left_column.php
@@ -10,23 +10,37 @@
 ?>
 <!-- Left Column -->
 <div class="w3-third w3-margin-top">
-	<div class="w3-white w3-text-grey w3-card-4  w3-border w3-border-black w3-round-large" style="height:auto;max-height:603px;overflow:auto;">
+	<div class="w3-white w3-text-grey w3-card-4  w3-border w3-border-black w3-round-large" style="height:auto;max-height:603px;min-height:603px;overflow:auto;">
 		
 		<div class="w3-bar w3-black w3-card w3-padding">
 			<a href="https://<?php echo $web; ?>" class="w3-bar-item" style="padding: 8px 5px;">
 				<image src="../images/system/logo.png" alt="NEUB LOGO" class="w3-image" style="width:100%;max-width:30px;">
 			</a>
-			<a href="index.php" class="w3-bar-item w3-xlarge w3-decoration-null" style="padding: 8px 3px;"><?php echo $title; ?></a>
+			<a href="f_index.php" class="w3-bar-item w3-xlarge w3-decoration-null" style="padding: 8px 3px;"><?php echo $title; ?></a>
 			
 		</div>
 		
+		<!-- Faculty ID info -->
+		<div class="w3-container w3-row w3-border w3-round-large w3-leftbar w3-rightbar" style="height: auto;margin: 20px 25px;padding: 10px 10px;">
+			<p class="w3-align-left w3-text-black w3-margin-0">Welcome, </p>
+			<div class="w3-col s3">
+				<img src="../images/system/male_profile.png" class="w3-circle w3-margin-right w3-margin-top" style="width:100%;max-width:70px;">
+			</div>
+			<div class="w3-col s8">
+				<p class="w3-align-left w3-text-black">
+					<strong>Noushad Sojib</strong>
+					</br>Assistant Professor
+					</br>Department of Computer Science & Engineering
+				</p>
+			</div>
+		</div>
 		
-		<div class="w3-bar-block w3-text-black" style="height: 493px;margin: 20px 25px;">
+		<div class="w3-bar-block w3-text-black" style="height: auto;margin: 20px 25px;">
 			<p class="w3-bold w3-xlarge w3-text-teal w3-bottombar" style="margin:0px 0px 15px 0px;"><i class="fa fa-folder-open-o"></i> Menu</p>
-			<a href="#" class="w3-bar-item w3-bold w3-decoration-null w3-hover-teal w3-round-large w3-border-teal w3-bottombar w3-leftbar"><i class="fa fa-dashboard"></i> Dashboard</a>
-			<a href="#" class="w3-bar-item w3-bold w3-decoration-null w3-hover-teal w3-round-large w3-border-teal w3-bottombar w3-leftbar w3-margin-top"><i class="fa fa-search"></i> Search Result</a>
-			<a href="#" class="w3-bar-item w3-bold w3-decoration-null w3-hover-teal w3-round-large w3-border-teal w3-bottombar w3-leftbar w3-margin-top"><i class="fa fa-edit"></i> Edit Profile</a>
-			<a href="log_out.php?log_out=yes" class="w3-bar-item w3-bold w3-decoration-null w3-hover-teal w3-round-large w3-border-teal w3-bottombar w3-leftbar w3-margin-top"><i class="fa fa-sign-out"></i> Sign Out</a>
+			<a href="#" class="w3-bar-item w3-bold w3-decoration-null w3-hover-black w3-round-large w3-border-teal w3-bottombar w3-leftbar"><i class="fa fa-dashboard"></i> Dashboard</a>
+			<a href="#" class="w3-bar-item w3-bold w3-decoration-null w3-hover-black w3-round-large w3-border-teal w3-bottombar w3-leftbar w3-margin-top"><i class="fa fa-search"></i> Search Result</a>
+			<a href="#" class="w3-bar-item w3-bold w3-decoration-null w3-hover-black w3-round-large w3-border-teal w3-bottombar w3-leftbar w3-margin-top"><i class="fa fa-edit"></i> Edit Profile</a>
+			<a href="log_out.php?log_out=yes" class="w3-bar-item w3-bold w3-decoration-null w3-hover-black w3-round-large w3-border-teal w3-bottombar w3-leftbar w3-margin-top"><i class="fa fa-sign-out"></i> Sign Out</a>
 		</div>
 		
 		
