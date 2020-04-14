9eee192941a06dbdd128c5e4a40a1a773cde54c0
Mir Lutfur Rahman <mirlutfur.rahman@gmail.com>
2020-04-14 23:35:22 +0600

    14.04.2020

--- includes/moderator_admin/navbar.php
+++ includes/moderator_admin/navbar.php
@@ -2,14 +2,14 @@
 <div class="w3-container w3-margin-0 w3-padding-0">
 	<div class="w3-bar w3-black w3-card w3-hide-medium w3-hide-small">
 		<a href="https://<?php echo $web; ?>" target="_blank" class="w3-bar-item">
-			<image src="../images/system/logo.png" alt="NEUB LOGO" class="w3-image" style="width:100%;max-width:90px;">
+			<image src="../images/system/<?php echo $logo; ?>" alt="NEUB LOGO" class="w3-image" style="width:100%;max-width:90px;">
 		</a>
 		<a href="index.php" class="w3-bar-item w3-jumbo w3-decoration-null"><?php echo $title; ?></a>
 		
 	</div>
 	<div class="w3-bar w3-black w3-card w3-hide-large w3-hide-small">
 		<a href="https://<?php echo $web; ?>" target="_blank" class="w3-bar-item">
-			<image src="../images/system/logo.png" alt="NEUB LOGO" class="w3-image" style="width:100%;max-width:75px;">
+			<image src="../images/system/<?php echo $logo; ?>" alt="NEUB LOGO" class="w3-image" style="width:100%;max-width:75px;">
 		</a>
 		</br>
 		<a href="index.php" class="w3-bar-item w3-xxxlarge w3-decoration-null" style="margin-top: -15px;"><?php echo $title; ?></a>
@@ -17,7 +17,7 @@
 	</div>
 	<div class="w3-bar w3-black w3-card w3-hide-large w3-hide-medium">
 		<a href="https://<?php echo $web; ?>" target="_blank" class="w3-bar-item">
-			<image src="../images/system/logo.png" alt="NEUB LOGO" class="w3-image" style="width:100%;max-width:55px;">
+			<image src="../images/system/<?php echo $logo; ?>" alt="NEUB LOGO" class="w3-image" style="width:100%;max-width:55px;">
 		</a>
 		</br>
 		<a href="index.php" class="w3-bar-item w3-xxlarge w3-decoration-null" style="margin-top: -15px;"><?php echo $title; ?></a>
